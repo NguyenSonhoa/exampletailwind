c69e2f34e7f7bad98abd2e853fe69efeb30a19f5
Nguyễn Hoàng Sơn <119683432+NguyenSonhoa@users.noreply.github.com>
2024-11-05 16:07:20 +0700

    PrimeLoginTailwindCSS
    This contains animation (just small) modern, cleans and smooth, dark mode, its stable for phone (checked), login example.

---
+++ tailwind login/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss-animated')
+  ],
+}
+
